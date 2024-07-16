c2f6a1c6dbfdf96bf4cc1136e2905febd946eafa
Samms206 <sh2069936@gmail.com>
2024-07-16 19:48:20 +0700

    Filtering Project

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,15 +1,52 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+
+    queryParams = queryParams || {}
+    const searchFieldChanged = (name, value) => {
+        if (value){
+            queryParams[name] = value
+        }else{
+            delete queryParams[name]
+        }
+
+        router.get(route('project.index'), queryParams);
+    };
+
+    const onKeyPress = (name, e) =>{
+        if(e.key !== 'Enter') return;
+        searchFieldChanged(name, e.target.value);
+    };
+
     return (
         <Authenticated user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>}>
             <Head title="Projects" />
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="w-full flex justify-between mb-4">
+                        <TextInput
+                            className=""
+                            defaultValue={queryParams.name}
+                            placeholder="Project Name"
+                            onBlur={e => searchFieldChanged('name', e.target.value)}
+                            onKeyPress={e => onKeyPress('name', e)} />
+
+                        <SelectInput
+                            className="" 
+                            defaultValue={queryParams.status}
+                            onChange={(e) => searchFieldChanged('status', e.target.value)}>
+                                <option value="">Select Status</option>
+                                <option value="pending">Pending</option>
+                                <option value="in_progress">In Progress</option>
+                                <option value="completed">Completed</option>
+                            </SelectInput>
+                    </div>
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
@@ -17,7 +54,7 @@ export default function Index({ auth, projects }) {
                                     <th className="px-3 py-2">ID</th>
                                     <th className="px-3 py-2">Image</th>
                                     <th className="px-3 py-2">Name</th>
-                                    <th className="px-3 py-2">Status</th>
+                                    <th className="px-3 py-2 text-center">Status</th>
                                     <th className="px-3 py-2">Create Date</th>
                                     <th className="px-3 py-2">Due Date</th>
                                     <th className="px-3 py-2">Created By</th>
@@ -32,9 +69,9 @@ export default function Index({ auth, projects }) {
                                             <img width={100} src={project.image_path} alt="" />
                                         </td>
                                         <td className="px-3 py-2">{project.name}</td>
-                                        <td className="px-3 py-2">
+                                        <td className="px-3 py-2 text-center text-nowrap">
                                             <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
-                                            {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                                {PROJECT_STATUS_TEXT_MAP[project.status]}
                                             </span>
                                         </td>
                                         <td className="px-3 py-2 text-nowrap">{project.created_at}</td>
@@ -48,7 +85,7 @@ export default function Index({ auth, projects }) {
                                 ))}
                             </tbody>
                         </table>
-                        <Pagination links={projects.meta.links}/>
+                        <Pagination links={projects.meta.links} />
                         {/* {console.log(projects.meta.links)} */}
                     </div>
                 </div>
