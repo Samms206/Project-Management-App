559b6d9a1de674707f4cc6a751e4f6c751c3e584
Samms206 <sh2069936@gmail.com>
2024-07-16 10:34:19 +0700

    Create controller & Define routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,7 +18,13 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'sam@example.com',
+            'password' => '12345678',
+            'email_verified_at' => time()
         ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
+
+
     }
 }
