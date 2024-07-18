7122fe05786e1460919aaeed61f7855a7049e1c7
Samms206 <sh2069936@gmail.com>
2024-07-18 19:37:18 +0700

    updated

--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -9,7 +9,7 @@ export default function Index({ auth, tasks, queryParams = null }) {
         <Authenticated user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Tasks</h2>}>
             <Head title="Tasks" />
-            <div className="py-12">
+            <div className="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <TasksTable tasks={tasks} queryParams={queryParams}/>
             </div>
         </Authenticated>
--- resources/js/Pages/Task/TasksTable.jsx
+++ resources/js/Pages/Task/TasksTable.jsx
@@ -38,7 +38,7 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
 
     return (
         <>
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div className="w-full">
                 <div className="w-full flex justify-between mb-4">
                     <TextInput
                         className=""
@@ -58,7 +58,7 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
                     </SelectInput>
                 </div>
                 <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg"></div>
-                <div className="overflow-auto">
+                <div className="overflow-auto rounded-lg">
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                             <tr className="text-nowrap">
